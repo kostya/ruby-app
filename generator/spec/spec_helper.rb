e3816053e28bd851b6ef7310dc8b8f2415748e69
Konstantin Makarchev <kostya27@gmail.com>
2012-06-22 01:15:00 +0400

    # spec helper template

--- generator/spec/spec_helper.rb
+++ generator/spec/spec_helper.rb
@@ -1,6 +1,17 @@
-require 'rubygems'
-require "bundler/setup"
+ENV["APP_ENV"] = 'test'
+require File.expand_path("../../config/environment", __FILE__)
 
-ENV['APP_ENV'] = 'test'
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.join(App.root, %w{spec support ** *.rb})].each {|f| require f}
 
-require File.join(File.dirname(__FILE__), %w{.. config environment})
+RSpec.configure do |config|
+  # ## Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+
+end
