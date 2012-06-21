23df272e1196049ec2410f9385f7e8a875598203
Konstantin Makarchev <kostya27@gmail.com>
2012-06-22 01:17:37 +0400

    boot

--- lib/ruby-app/boot.rb
+++ lib/ruby-app/boot.rb
@@ -1,6 +1,8 @@
 $KCODE='u' if RUBY_VERSION < '1.9'
 
+local_root = File.dirname(__FILE__)
+
 # app from gem
-require File.join(File.dirname(__FILE__), 'application')
+require File.join(local_root, 'application')
 
 $:.unshift(File.join(App.root, %w{ lib }))
