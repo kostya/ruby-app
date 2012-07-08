e8099fa3a44fc19b0f6ed6922245abbd72555ec7
Konstantin Makarchev <kostya27@gmail.com>
2012-07-09 01:27:11 +0400

    extend application

--- lib/ruby-app/boot.rb
+++ lib/ruby-app/boot.rb
@@ -2,7 +2,11 @@ $KCODE='u' if RUBY_VERSION < '1.9'
 
 local_root = File.dirname(__FILE__)
 
+# define class if undefined
+class Application
+end
+
 # app from gem
-require File.join(local_root, 'application')
+require File.join(local_root, 'application_defaults')
 
 $:.unshift(File.join(App.root, %w{ lib }))
