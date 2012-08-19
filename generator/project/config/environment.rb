f65ceaabe8089e99dad298b6a7e266128d31511e
Konstantin Makarchev <kostya27@gmail.com>
2012-08-19 21:05:31 +0400

    # comment in generator

--- generator/project/config/environment.rb
+++ generator/project/config/environment.rb
@@ -1,6 +1,8 @@
 # -*- encoding : utf-8 -*-
 require File.join(File.dirname(__FILE__), 'boot')
 
-# ENV['APP_ENV'] = 'production' # uncomment if need forced env
+# uncomment if needed force env
+# ENV['APP_ENV'] = 'production'
+# or create file config/environment.current (by captistrano for example) and put there string 'production'
 
 require 'ruby-app/environment'
