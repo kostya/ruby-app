a7ce559b4ea5b050faad5702882cbd4e7357c8cf
Konstantin Makarchev <kostya27@gmail.com>
2013-09-10 23:55:39 +0400

    fix formatter

--- lib/ruby-app/local_logger.rb
+++ lib/ruby-app/local_logger.rb
@@ -2,8 +2,13 @@
 require 'logger'
 
 class LocalLogger < Logger
+  FORMAT = '%d.%m.%Y %H:%M:%S'
+  
   def initialize(*a)
     super
-    self.formatter = lambda { |s, d, p, m| "#{d.strftime("%d.%m.%Y %H:%M:%S")} #{m}\n" }
+    
+    self.formatter = Proc.new do |s, d, p, m|
+      "#{d.strftime(FORMAT)} #{s.ljust(5)} -- #{m}\n"
+    end                        
   end
 end
